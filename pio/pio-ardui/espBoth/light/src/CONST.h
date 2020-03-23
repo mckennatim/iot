93990a3ec354d99463f88a86fde0c72da1479854
mckennatim <mckenna.tim@gmail.com>
2020-03-23 19:55:57 -0400

    light-config

--- pio/pio-ardui/espBoth/light/src/CONST.h
+++ pio/pio-ardui/espBoth/light/src/CONST.h
@@ -6,25 +6,12 @@
   #include <ESP8266WebServer.h>
 	
   #define sizeOf(a) (sizeof(a) / sizeof(a[0]))
-  
-  // extern ESP8266WebServer server;
-  // extern BH1750 lightMeter;
-
-  // extern OneWire oneWire(4);
-  // extern DallasTemperature DS18B20(&oneWire);
 
   struct prg_t{
     int id;
     AlarmID_t aid;
     int ev;
     int prg[11][5];//max 11 events [hr,min,max,min]  
-  };
-
-  struct sensor_t{
-    int id;
-    char type[12];
-    char model[12];
-    int inport;
   };
 
   struct senso_t{
@@ -39,29 +26,22 @@
     int numsens;
     senso_t se[4];
   };
+  static sen_t SE {
+    4,//number of different sensors
+    7,//number of sensors
+    {{1, {0}, "light", "BH1750"},//assumes 
+    {2, {1,2}, "temp", "DS18B20a"},  
+    {2, {3,4}, "temp", "DS18B20b"},  
+    {2, {5,6}, "temp-hum", "DHT22"}}
+  };
 
   struct portsin_t{
     int DS18B20a;
     int DS18B20b;
     int DHT22a;
   };
+  static portsin_t inpo {4, 5, 14};
 
-  portsin_t inpo {4, 5, 14};
-
-  sen_t SE {
-    4,//number of different sensors
-    7,//number of sensors
-    {{1, {0}, "light", "BH1750"},
-    {2, {1,2}, "temp", "DS18B20a"},  
-    {2, {3,4}, "temp", "DS18B20b"},  
-    {2, {5,6}, "temp-hum", "DHT22"}}
-  };
-
-  sensor_t SA[3] ={
-    {0, "light", "BH1750", 4},
-    {1, "temp", "DS18B20", 5},  
-    {2, "temp", "DS18B20", 5}
-  };
 
   struct ctrld_sensor_t{
     int id;
@@ -79,57 +59,44 @@
     prg_t prgo;  
   };
 
-  struct sr_t{
-    int numsrs[3];
-    char srlst[4][3];
-    sensor_t se[2];
-    ctrld_sensor_t cs[1];
-    timr_t ti[1];
-  };
+  // struct server_t{
+  //   char owner[254];
+  //   char pwd[24];
+  //   char mqtt_server[60];
+  //   char mqtt_port[6];
+  // };  
 
-  sr_t SRS {
-    {2, 1, 1},
-    {"se", "se", "cs", "ti"},
-    {
-      {0, "light", "BH1750", 4},
-      {1, "temp", "DS18B20", 5}
-    },
-    {
-      {2, "temp", "DS18B20", 5, 12, 
-        {2, 255, 1, {{0,0,65,63}}}
-      }
-    },
-    {
-      {3, "timer", 13,
-        {3, 255, 1, {{0,0,1}}}
-      }
-    }
-  };
-
-  struct server_t{
-    char owner[254] ;
-    char pwd[24];
-    char mqtt_server[60];
-    char mqtt_port[6];
-  };  
-
-  server_t COMP = {
-    "tim@sitebuilt.net",
-    "geniot",
-    "sitebuilt.net",
-    "1884"
-  };
+  // server_t COMP = {
+  //   "tim@sitebuilt.net",
+  //   "geniot",
+  //   "sitebuilt.net",
+  //   "1884"
+  // };
 
   struct labels_t {
     char scribedTo[6][8]; 
     int numcmds;
   };
 
-  labels_t LA {
+  static labels_t LA {
     {"devtime", "cmd", "prg", "req", "set", "progs"},
     6
   };
 
+  // #define OWNER "tim@sitebuilt.net"
+  // #define PWD "geniot"
+  // #define MQTT_SERVER "sitebuilt.net"
+  // #define MQTT_PORT "1884"
+
+  // extern char* OWNER ="tim@sitebuilt.net";
+  // extern const char* PWD ="geniot";
+  // extern const char* MQTT_SERVER= "sitebuilt.net";
+  // extern const char* MQTT_PORT= "1884";
+
+  // extern char owner[254] ="tim@sitebuilt.net";
+  // extern char pwd[24] ="geniot";
+  // extern char mqtt_server[60]= "sitebuilt.net";
+  // extern char mqtt_port[6]= "1884";
 
 
 #endif
--- pio/pio-ardui/espBoth/light/src/main.cpp
+++ pio/pio-ardui/espBoth/light/src/main.cpp
@@ -5,6 +5,7 @@
 #include <BH1750.h>
 #include <DHT.h>
 #include "CONST.h"
+#include "config.h"
 
 
 ESP8266WebServer server;
@@ -20,6 +21,8 @@ DallasTemperature DS18B20b(&oneWireb);
 #define DHTPIN inpo.DHT22a
 DHT dht22(DHTPIN, DHTTYPE);
 
+
+
 void initShit(){
   for (int i=0;i<SE.len;i++){
     Serial.println(SE.se[i].model);
@@ -30,14 +33,6 @@ void initShit(){
     }
   }  
 }
-
-void doWhat(int sr){
-  Serial.print(sr);
-  Serial.println(F("dowhat baby assume it is a request for the reading"));
-  //const char setyp= SRS.srlst[sr];
-  Serial.println(SRS.srlst[sr]);
-} 
-
 
 void readSensors(){
   int senvals[SE.numsens];
@@ -76,28 +71,9 @@ void readSensors(){
 void setup(){
   Serial.begin(115200);
   initShit();
-  // Initialize the I2C bus (BH1750 library doesn't do this automatically)
-  //Wire.begin();
-  // On esp8266 you can select SCL and SDA pins using Wire.begin(D4, D3);
-  //lightMeter.begin();
-  //Serial.println(F("BH1750 light begin"));
-  // Serial.println(SRS.se[0].id);
-  // Serial.println(SRS.se[0].type);
-  // Serial.println(SRS.se[0].model);
-  // Serial.println(SRS.se[1].type);
-  // Serial.println(SRS.se[1].model);
-  // Serial.println(SRS.cs[0].prgo.prg[0][2]);
-  // Serial.println(SRS.cs[0].prgo.prg[0][2]);
-  // Serial.println(SRS.ti[0].prgo.aid);
-  // Serial.println(SRS.ti[0].prgo.aid);
-
-  Serial.println(COMP.owner);
+  getOnline();//config.cpp
   Serial.println(LA.scribedTo[0]);
-  Serial.println(SA[1].model);
-
-  doWhat(2);
-  readSensors();
-
+  // printf("%s \n", OWNER);
 }
 
 
